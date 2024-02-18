2984600c6cec00401ab055c1651b71bc954953a7
s.masoumbeigi <sbeygi712@gmail.com>
2024-02-18 15:05:41 +0330

    widget modifications

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,19 +14,44 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
+    final inputDecoration = InputDecoration(
+      fillColor: Colors.grey[600],
+      filled: true,
+      border: OutlineInputBorder(
+        borderSide: BorderSide.none,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Center(
         child: Form(
           key: _formKey,
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.w),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextFormField(),
-                TextFormField(),
-                ElevatedButton(onPressed: () {}, child: const Text('Login')),
-              ],
+            child: Card(
+              color: Colors.grey[900]!.withOpacity(0.5),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(decoration: inputDecoration).animate().slideX(
+                          duration: 1.seconds,
+                        ),
+                    TextFormField(decoration: inputDecoration,)
+                        .animate()
+                        .slideX(delay: 1.seconds, duration: 1.seconds),
+                    ElevatedButton(onPressed: () {}, child: const Text('Login'))
+                        .animate()
+                        .fadeIn(delay: 2.seconds, duration: 1.seconds)
+                        .shimmer(
+                            duration: 10.seconds,
+                            color: Colors.grey.withOpacity(0.6)),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
