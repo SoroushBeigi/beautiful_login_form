a8ab81fa321eae4a1bbcae36840a2be1056559d2
s.masoumbeigi <sbeygi712@gmail.com>
2024-03-02 16:41:36 +0330

    creating home page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -33,13 +33,15 @@ class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     final inputDecoration = InputDecoration(
-      hintStyle: const TextStyle(color: Colors.grey,),
+      hintStyle: const TextStyle(
+        color: Colors.grey,
+      ),
       border: OutlineInputBorder(
         borderSide: BorderSide(color: Colors.grey[200]!),
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
-    const textStyle = TextStyle(color: Colors.white,letterSpacing: 2);
+    const textStyle = TextStyle(color: Colors.white, letterSpacing: 2);
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -81,43 +83,52 @@ class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
                   sigmaY: 5,
                 ),
                 child: Container(
-   margin: EdgeInsets.symmetric(horizontal:4.w),
+                  margin: EdgeInsets.symmetric(horizontal: 4.w),
                   child: Card(
-                      color: Colors.black.withOpacity(0.3),
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(vertical:6.h,horizontal:5.w),
-                        child: Form(
-                            key: _formKey,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                TextFormField(
-                                  decoration: inputDecoration.copyWith(hintText: 'Enter your email address'),
-                                  style: textStyle,
-                                  keyboardType: TextInputType.emailAddress,
-                                ),
-                                SizedBox(
-                                  height: 3.h,
-                                ),
-                                TextFormField(
-                                  decoration: inputDecoration.copyWith(hintText: 'Enter your password'),
-                                  style: textStyle,
-                                  keyboardType:TextInputType.visiblePassword,
-                                  obscureText: true,
-                                ),
-                                SizedBox(height: 4.h),
-                                SizedBox(
-                                  width: 40.w,
-                                    height:7.5.h,
-                                  child: ElevatedButton(
-                                    onPressed: () {},
-                                    child: const Text('Login'),
-                                  ).animate().fadeIn(delay: 2.seconds,duration: 1.seconds,curve: Curves.easeIn),
-                                )
-                              ],
-                            )),
-                      ),).animate().slideX(duration: 2.seconds,curve: Curves.ease),
+                    color: Colors.black.withOpacity(0.3),
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 6.h, horizontal: 5.w),
+                      child: Form(
+                          key: _formKey,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              TextFormField(
+                                decoration: inputDecoration.copyWith(
+                                    hintText: 'Enter your email address'),
+                                style: textStyle,
+                                keyboardType: TextInputType.emailAddress,
+                              ),
+                              SizedBox(
+                                height: 3.h,
+                              ),
+                              TextFormField(
+                                decoration: inputDecoration.copyWith(
+                                    hintText: 'Enter your password'),
+                                style: textStyle,
+                                keyboardType: TextInputType.visiblePassword,
+                                obscureText: true,
+                              ),
+                              SizedBox(height: 4.h),
+                              SizedBox(
+                                width: 40.w,
+                                height: 7.5.h,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context).push();
+                                  },
+                                  child: const Text('Login'),
+                                ).animate().fadeIn(
+                                    delay: 2.seconds,
+                                    duration: 1.seconds,
+                                    curve: Curves.easeIn),
+                              )
+                            ],
+                          )),
+                    ),
+                  ).animate().slideX(duration: 2.seconds, curve: Curves.ease),
                 ),
               ),
             ),
